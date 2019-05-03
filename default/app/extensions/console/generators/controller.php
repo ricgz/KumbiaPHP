a3521c17279d6df81c35011777561e67c5808fdb
Ricardo GZ <ricardo.gonzalez.zuniga87@gmail.com>
2019-05-03 11:06:08 -0400

    se crean consolas personalizadas de model y controller en app/extensions/console y se elimina archivo empty

---
+++ default/app/extensions/console/generators/controller.php
@@ -0,0 +1,27 @@
+/**
+ * @file <?= $file_name.PHP_EOL ?>
+ * @category App
+ * @package Controllers
+ *
+ * @author Ricardo Gonzalez Z.
+ * @date <?= $created.PHP_EOL ?>
+ * @brief 
+ */
+class <?= $class ?>Controller extends AppController
+{
+    /**
+    * Descripcion corta
+    *
+    * Descripcion Larga
+    *
+    * @param int $id y descripcion
+    *
+    * @return void
+    */
+    public function index(){
+        // inicio codigo para eliminar
+        echo "Cree un archivo index.phtml en view <?= $controller ?>";
+        exit;
+        // fin codigo eliminar.
+    }
+}
---
+++ default/app/extensions/console/generators/model.php
@@ -0,0 +1,23 @@
+/**
+ * @file <?= $file_name.PHP_EOL ?>
+ * @category App
+ * @package Models
+ *
+ * @author  Ricardo Gonzalez Z.
+ * @date <?= $created.PHP_EOL ?>
+ * @brief 
+ */
+
+class <?= $class ?> extends ActiveRecord
+{   
+    /**
+    * Constructor e inicializador del Modelo
+    *
+    * Descripcion Larga
+    *
+    * @return void
+    */
+    function initialize(){
+        // indicar relaciones de BBDD
+    }
+}
